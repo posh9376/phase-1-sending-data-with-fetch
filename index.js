f0919bbcb9b9a04b3169c132ac011ddb22770b12
Eric Richard <reric0802@gmail.com>
2024-10-14 00:07:43 +0300

    added  js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submiData(name, mail) {
+    let userData = {
+        username:` ${name}`,
+        email:`${mail}`
+    }
+
+    let ft = fetch('http://localhost:3000/users', {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(res => res.json())
+    .then(object => {
+        let id = object.id
+        console.log(id);
+        let div = document.querySelector('.idBox')
+        newId = document.createElement('h2')
+        newId.textContent = `The new id is : ${id}`
+        div.appendChild(newId)
+    })
+    .catch((error) => {
+        let err = document.createElement('h3')
+        err.textContent = `${error.message}`
+        let div = document.querySelector('.idBox')
+        div.appendChild(err)
+    });
+    return ft
+}
